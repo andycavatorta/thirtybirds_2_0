88d677897450a8926b2154e2c9607cc63a8f7105
Andy Cavatorta <andycavatorta@gmail.com>
2017-05-02 16:36:11 -0400

    fixing whitespace

--- Network/email_simple.py
+++ Network/email_simple.py
@@ -8,30 +8,30 @@ from email.MIMEText import MIMEText
 
 class Send():
     def __init__(self,smptServer='smtp.gmail.com', smtpPort=587, fromAddress="", password=""):
-    	try:
-	        self.safety = True
-	        self.messages = []
-	        self.smptServer = smptServer
-	        self.smtpPort = smtpPort
-	        self.fromAddress = fromAddress
-	        self.password = password
-	    except Exception as e:
-	    	print "Exception in email_simple.Send.__init", e
+    try:
+        self.safety = True
+        self.messages = []
+        self.smptServer = smptServer
+        self.smtpPort = smtpPort
+        self.fromAddress = fromAddress
+        self.password = password
+    except Exception as e:
+        print "Exception in email_simple.Send.__init", e
 
     def send(self, toAddress, subject, body):
-    	try:
-			msg = MIMEMultipart()
-			msg['From'] = self.fromAddress
-			msg['To'] = toAddress
-			msg['Subject'] = subject
-			msg.attach(MIMEText(body, 'plain'))
-			server = smtplib.SMTP(self.smptServer, self.smtpPort)
-			server.starttls()
-			server.login(self.fromAddress, self.password)
-			server.sendmail(self.fromAddress, toAddress, msg.as_string())
-			server.quit()
-	    except Exception as e:
-	    	print "Exception in email_simple.Send.send", e
+        try:
+            msg = MIMEMultipart()
+            msg['From'] = self.fromAddress
+            msg['To'] = toAddress
+            msg['Subject'] = subject
+            msg.attach(MIMEText(body, 'plain'))
+            server = smtplib.SMTP(self.smptServer, self.smtpPort)
+            server.starttls()
+            server.login(self.fromAddress, self.password)
+            server.sendmail(self.fromAddress, toAddress, msg.as_string())
+            server.quit()
+        except Exception as e:
+            print "Exception in email_simple.Send.send", e
 
 
 def init(fa, pw):
