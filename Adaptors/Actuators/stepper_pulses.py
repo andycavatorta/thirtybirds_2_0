30c1e1fd1c3ea25213433c658581357c2f55cd15
Andy Cavatorta <andycavatorta@gmail.com>
2018-02-17 23:00:05 -0500

    typo

--- Adaptors/Actuators/stepper_pulses.py
+++ Adaptors/Actuators/stepper_pulses.py
@@ -4,7 +4,7 @@ import threading
 import time
 
 class Channel(threading.Thread):
-    def __init__(self, pulse_pin, dir_pin, base_pulse_period = 0.0001, steps_finished_callback = False, backwards_orientation = False):
+    def __init__(self, pulse_pin, dir_pin, base_pulse_period = 0.001, steps_finished_callback = False, backwards_orientation = False):
         threading.Thread.__init__(self)
         self.queue = Queue.Queue()
         self.pulse_pin = pulse_pin
@@ -81,6 +81,21 @@ def init(channel_data):
             False if "backwards_orientation" not in channel else channel["backwards_orientation"]
         )
         channels[channel["name"]].start()
+
+def set(channel_name, action, data):
+    if channel_name not in channels:
+        print "Channel name not found:", channel_name
+        return
+    if action == "steps":
+        channels[channel_name].set_steps(data)
+        return
+    if action == "speed":
+        channels[channel_name].set_speed(data)
+        return
+    if action == "enable":
+        channels[channel_name].set_enable(data)
+        return
+    print "Action not found:", action
 
 # usage:
 # channels["channel_name"].set_speed(-0.5)
